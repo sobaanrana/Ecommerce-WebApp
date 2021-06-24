9d4bb9ca38125177d4efd307bcc88072279683c2
Sobaan <55842323+sobaanrana@users.noreply.github.com>
2021-06-24 22:52:07 +0500

    Commit

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,29 @@
+/*
+import { ADD_TO_CART } from "../constants/cartConstants"
+
+export const cartReducer = (state = {cartItems : []} , action) => { //cartItems will contain whatever user has selected
+    switch(type.action)  {
+        
+        case ADD_TO_CART:
+            const item = action.payload;
+            /*const isItemExist = state.cartItems.find(i => i.product === item.product) //
+
+            if(isItemsExist) {
+                return {
+                    ...state,
+                    cart
+                }
+
+
+            } else {
+                ...state,
+                cartItems: [...state.cartItems, item]
+
+            }
+        default :
+            return state
+
+    }
+}
+
+*/
