3e9ad0d5bb2e97db6a09bde3329976f9c9499d1b
Sobaan <55842323+sobaanrana@users.noreply.github.com>
2021-06-24 21:09:42 +0500

    Commit

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+//var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productsRouter = require('./routes/products');
+var ordersRouter = require('./routes/orders');
+
+
 var mongoose = require('mongoose');
 var config = require('config');
+//const bodyparser = require('body-parser);
+const cloudinary = require('cloudinary');
 
 var app = express();
 
@@ -20,11 +25,21 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//app.use(bodyparser.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/', indexRouter);
+app.use('/user', usersRouter);
 app.use('/products', productsRouter);
+app.use('/orders', ordersRouter);
+
+//Setting up cloudinary configuration
+cloudinary.config({
+  cloud_name : config.get("cloudinary_cloud_name"),
+  api_key : config.get("cloudinary_api_key"),
+  api_secret : config.get("cloudinary_api_secret")
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
