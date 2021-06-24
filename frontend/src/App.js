6737e9688dc0285324c257cf4d4250b5f3175c52
Sobaan <55842323+sobaanrana@users.noreply.github.com>
2021-06-24 21:13:07 +0500

    Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,30 @@ import Footer from './Components/Layout/Footer';
 import Header from './Components/Layout/Header';
 import Home from './Components/Home'
 import ProductDetails from './Components/Layout/productDetails';
+import Login from './Components/Login';
+import Register from './Components/Layout/Register';
+
+import { loadUser} from './actions/userActions';
+import { useEffect } from 'react';
+import store from './store';
 
 function App() {
+
+  /*
+  useEffect(() => {
+    store.dispatch(loadUser())
+  })*/
   return (
     <Router>
       <div className="App">
         <Header />
         <div className="container container-fluid">
           <Route path='/' component={Home} exact></Route>
+          <Route path='/Search/:keyword' component={Home} ></Route>
           <Route path='/product/:id' component={ProductDetails} exact></Route>
+          <Route path='/user/login' component={Login} exact></Route>
+          <Route path='/user/register' component={Register} exact></Route>
+          
         </div>
         <Footer />
       </div>
@@ -20,5 +35,6 @@ function App() {
     
   );
 }
+
 
 export default App;
