bac90432451eee887b465690914afecc5d42249f
Sobaan <55842323+sobaanrana@users.noreply.github.com>
2021-06-24 22:51:53 +0500

    Commit

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,13 @@
+import { ADD_TO_CART } from "../constants/cartConstants"
+/*
+export const addToCart = () => async (dispatch) => {
+    dispatch({type: ALL_PRODUCTS_REQUEST}) //when it is dispatched it set loading true and products to empty array in the state
+
+        const { data } = await axios.get('/products/${id}') //Get request
+
+        dispatch({
+            type: ADD_TO_CART,
+            payload: data.product
+        })
+}
+*/
