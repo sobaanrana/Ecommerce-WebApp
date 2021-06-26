7819e02ed0ff6fd8d7fdf6eb00bc65fde01304ec
Sobaan <55842323+sobaanrana@users.noreply.github.com>
2021-06-26 17:44:25 +0500

    Update FormChange.jsx

--- frontend/src/Components/Admin/FormChange.jsx
+++ frontend/src/Components/Admin/FormChange.jsx
@@ -26,91 +26,91 @@ const FormChange = () => {
                                         <div className="modal-body">
 
                                         <div className="container container-fluid">
-        <div className="wrapper my-5"> 
-        <form className="shadow-lg" encType='multipart/form-data'>
+                                            <div className="wrapper my-5"> 
+                                            <form className="shadow-lg" encType='multipart/form-data'>
 
-            <div className="form-group">
-              <label for="name_field">Name</label>
-              <input
-                type="text"
-                id="name_field"
-                className="form-control"
-                value=""
-              />
-            </div>
+                                                <div className="form-group">
+                                                <label for="name_field">Name</label>
+                                                <input
+                                                    type="text"
+                                                    id="name_field"
+                                                    className="form-control"
+                                                    value=""
+                                                />
+                                                </div>
 
-            <div className="form-group">
-                <label for="price_field">Price</label>
-                <input
-                  type="text"
-                  id="price_field"
-                  className="form-control"
-                  value=""
-                />
-              </div>
+                                                <div className="form-group">
+                                                    <label for="price_field">Price</label>
+                                                    <input
+                                                    type="text"
+                                                    id="price_field"
+                                                    className="form-control"
+                                                    value=""
+                                                    />
+                                                </div>
 
-              <div className="form-group">
-                <label for="description_field">Description</label>
-                <textarea className="form-control" id="description_field" rows="8" ></textarea>
-              </div>
+                                                <div className="form-group">
+                                                    <label for="description_field">Description</label>
+                                                    <textarea className="form-control" id="description_field" rows="8" ></textarea>
+                                                </div>
 
-              <div className="form-group">
-                <label for="category_field">Category</label>
-                <select className="form-control" id="category_field">
-                    <option>Electronics</option>
-                    <option>Home</option>
-                    <option>Others</option>
-                  </select>
-              </div>
-              <div className="form-group">
-                <label for="stock_field">Stock</label>
-                <input
-                  type="number"
-                  id="stock_field"
-                  className="form-control"
-                  value=""
-                />
-              </div>
+                                                <div className="form-group">
+                                                    <label for="category_field">Category</label>
+                                                    <select className="form-control" id="category_field">
+                                                        <option>Electronics</option>
+                                                        <option>Home</option>
+                                                        <option>Others</option>
+                                                    </select>
+                                                </div>
+                                                <div className="form-group">
+                                                    <label for="stock_field">Stock</label>
+                                                    <input
+                                                    type="number"
+                                                    id="stock_field"
+                                                    className="form-control"
+                                                    value=""
+                                                    />
+                                                </div>
 
-              <div className="form-group">
-                <label for="seller_field">Seller Name</label>
-                <input
-                  type="text"
-                  id="seller_field"
-                  className="form-control"
-                  value=""
-                />
-              </div>
-              
-              <div className='form-group'>
-                <label>Images</label>
-                
-                    <div className='custom-file'>
-                        <input
-                            type='file'
-                            name='product_images'
-                            className='custom-file-input'
-                            id='customFile'
-                            multiple
-                        />
-                        <label className='custom-file-label' for='customFile'>
-                            Choose Images
-                        </label>
-                    </div>
-            </div>
+                                                <div className="form-group">
+                                                    <label for="seller_field">Seller Name</label>
+                                                    <input
+                                                    type="text"
+                                                    id="seller_field"
+                                                    className="form-control"
+                                                    value=""
+                                                    />
+                                                </div>
+                                                
+                                                <div className='form-group'>
+                                                    <label>Images</label>
+                                                    
+                                                        <div className='custom-file'>
+                                                            <input
+                                                                type='file'
+                                                                name='product_images'
+                                                                className='custom-file-input'
+                                                                id='customFile'
+                                                                multiple
+                                                            />
+                                                            <label className='custom-file-label' for='customFile'>
+                                                                Choose Images
+                                                            </label>
+                                                        </div>
+                                                </div>
 
-  
-            <button
-              id="login_button"
-              type="submit"
-              class="btn btn-block py-3"
-            >
-              CREATE
-            </button>
+                                    
+                                                <button
+                                                id="login_button"
+                                                type="submit"
+                                                class="btn btn-block py-3"
+                                                >
+                                                CREATE
+                                                </button>
 
-          </form>
-    </div>
-</div>
+                                            </form>
+                                        </div>
+                                    </div>
 
                                             
 
