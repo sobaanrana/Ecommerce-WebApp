687d7592def6bb86f9c43e920a4501695fd9a229
Sobaan <55842323+sobaanrana@users.noreply.github.com>
2021-06-26 17:45:26 +0500

    Update Footer.jsx

--- frontend/src/Components/Layout/Footer.jsx
+++ frontend/src/Components/Layout/Footer.jsx
@@ -1,17 +1,31 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { contactUs } from '../../actions/userActions';
 
 const Footer = () => {
+
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('')
+
+  const submitHandler = (e) => {
+    dispatch(contactUs(email, message))
+  }
     return ( 
     
       <Fragment>
         <div className="container-fluid text-white footer">
           <div className="row">
-            <div className="col-lg-4 col-md-4 col-sm-10 col-12 d-block m-auto">
+            <div className="col-lg-4 col-md-4 col-sm-10 col-12 d-block m-auto aboutus">
               <h2>ABOUT US</h2>
               <p>
                 We provide best quality burger with our speacial recipies.All of our chefs are speacially trained from foreign countries.  We never compromise on quality and quantity.
               </p>
 
+            </div>
+
+            <div className="col-lg-4 col-md-4 col-sm-10 col-12 d-block m-auto">
+              <h2 className="socialsh2">FOLLOW US</h2>
               <ul className="socials">
                 <li><a href="#" target="_blank"><i className="fa fa-facebook"></i></a></li>
                 <li><a href="#" target="_blank"><i className="fa fa-twitter"></i></a></li>
@@ -21,23 +35,18 @@ const Footer = () => {
               </ul>
             </div>
 
-            <div className="col-lg-4 col-md-4 col-sm-10 col-12 d-block m-auto text-center address">
-              <h2>ADDRESS</h2>
-              <pre className="text-white"><i className="fa fa-map-marker"></i> : Shop # 01 ,Lahore</pre>
-              <pre className="text-white"><i className="fa fa-phone"></i> : 0077889966</pre>
-              <pre className="text-white"><i className="fa fa-envelope"></i> : restaurant@gmail.com</pre>
-            </div>
+          
 
             <div className="col-lg-4 col-md-4 col-sm-10 col-12 d-block m-auto contactus">
               <h2>CONTACT US</h2>
-              <form action="#">
+              <form onSubmit={submitHandler}>
                 <div className="email">
                   <p>Email *</p>
-                  <input type="email" required />
+                  <input type="email" required  value={email} onChange={(e) => setEmail(e.target.value)}/>
                 </div>
                 <div className="msg">
                   <p className="text">Message *</p>
-                  <input type="email" required />
+                  <textarea type="text" required value={message} onChange={(e) => setMessage(e.target.value)}/>
                 </div>
                 <button type="submit" className="btn btn-default btn-md submitbutton">Send</button>
               </form>
