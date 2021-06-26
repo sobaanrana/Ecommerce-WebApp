6e7f13e606ef6776565fa2d71744833ac2c875f7
Sobaan <55842323+sobaanrana@users.noreply.github.com>
2021-06-26 17:43:56 +0500

    Update cartReducers.js

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,29 +1,39 @@
-/*
-import { ADD_TO_CART } from "../constants/cartConstants"
+
+import { ADD_TO_CART, REMOVE_ITEM_CART } from "../constants/cartConstants"
 
 export const cartReducer = (state = {cartItems : []} , action) => { //cartItems will contain whatever user has selected
-    switch(type.action)  {
+    switch(action.type)  {
         
         case ADD_TO_CART:
             const item = action.payload;
-            /*const isItemExist = state.cartItems.find(i => i.product === item.product) //
+            const isItemExist = state.cartItems.find(i => i.product === item.product) //ids of project are checked
 
-            if(isItemsExist) {
+            if(isItemExist) {
                 return {
                     ...state,
-                    cart
+                    cartItems: state.cartItems.map(i => i.product === isItemExist.product ? item :
+                        i)
                 }
 
 
             } else {
-                ...state,
-                cartItems: [...state.cartItems, item]
+                return{
+                    ...state,
+                    cartItems: [...state.cartItems, item] //from whatever in array of cart items, take item
+
+                }
+              
 
             }
+        
+        case REMOVE_ITEM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(i => i.product !== action.payload)//filter all the products instead this 
+            }
+
         default :
             return state
 
     }
 }
-
-*/
