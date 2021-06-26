1dcfac5398b0e8aa4dac8ac3a821e549b8121f9d
Sobaan <55842323+sobaanrana@users.noreply.github.com>
2021-06-26 17:56:57 +0500

    Update cartActions.js

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,19 +1,4 @@
-/*import { ADD_TO_CART } from "../constants/cartConstants"
-
-export const addToCart = () => async (dispatch) => {
-    dispatch({type: ALL_PRODUCTS_REQUEST}) //when it is dispatched it set loading true and products to empty array in the state
-
-        const { data } = await axios.get('/products/${id}') //Get request
-
-        dispatch({
-            type: ADD_TO_CART,
-            payload: data.product
-        })
-}
-*/
-
 import axios from 'axios'
-//import { ADD_TO_CART, REMOVE_ITEM_CART,  } from '../constants/cartConstants'
 import { ADD_TO_CART, REMOVE_ITEM_CART,SAVE_SHIPPING_INFO } from "../constants/cartConstants"
 export const addItemToCart = (id, quantity) => async (dispatch, getState) => { //getState will get current state
     const { data } = await axios.get(`/products/${id}`)
